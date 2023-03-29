69bdc8ba05f6bcc98929529674ae9880c8b2f48e
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2023-03-29 16:19:03 +0700

    added support for passover arguments

--- remote.go
+++ remote.go
@@ -37,17 +37,35 @@ func main() {
 			continue
 		}
 		if e == "-h" || e == "--help" {
-			println("Usage: remote [-h|--help] [-f|--file REMOTE FILE] command")
+			println("Usage: remote [-h|--help] [-f|--file REMOTE FILE] command -- [passover arguments]")
 			println("    -h | --help               : show help and exit")
 			println("    -f | --file REMOTE FILE   : use REMOTE FILE as the script to run from; default to `remote.toml`")
 			println("    command                   : the script name or command to run")
+			println("    passover arguments        : arguments to pass over to the script, multiple -- can be used to chain passover arguments")
 			return
 		} else if (e == "-f" || e == "--file") && (i+1+1 < len(os.Args)) {
 			has_filename = true
 			filename = os.Args[i+1+1]
 			skipNext = true
+		} else if e == "--" {
+			// passover args
+			break
 		} else {
 			scriptName = os.Args[i+1]
+		}
+	}
+
+	var pass_over_args []string
+	var in_pass_over bool
+
+	for _, e := range os.Args[1:len(os.Args)] {
+		if e == "--" && !in_pass_over {
+			in_pass_over = true
+			continue
+		}
+
+		if in_pass_over {
+			pass_over_args = append(pass_over_args, e)
 		}
 	}
 
@@ -110,20 +128,26 @@ func main() {
 
 	// check if script is array
 	if _, ok := script.([]interface{}); ok {
-		execCommand(script.([]interface{}))
+		execCommand(script.([]interface{}), pass_over_args)
 	} else {
 		// script is struct
-		execCommandAndNext(script.(*toml.Tree), scripts)
+		execCommandAndNext(script.(*toml.Tree), scripts, pass_over_args)
 	}
 }
 
-func execCommand(command []interface{}) {
+func pop[T any](arr []T) (T, []T) {
+	return arr[0], arr[1:]
+}
+
+func execCommand(command []interface{}, extra_args []string) {
 	// convert to string array
 	var args []string
 
 	for _, arg := range command {
 		args = append(args, arg.(string))
 	}
+
+	args = append(args, extra_args...)
 
 	// execute command
 	cmd := exec.Command(args[0], args[1:]...)
@@ -138,7 +162,7 @@ func execCommand(command []interface{}) {
 	}
 }
 
-func execCommandAndNext(command *toml.Tree, scripts *toml.Tree) {
+func execCommandAndNext(command *toml.Tree, scripts *toml.Tree, extra_args []string) {
 	// get command
 	cmd := command.Get("command").([]interface{})
 
@@ -147,6 +171,19 @@ func execCommandAndNext(command *toml.Tree, scripts *toml.Tree) {
 
 	for _, arg := range cmd {
 		args = append(args, arg.(string))
+	}
+
+	// for this one, do not just copy, but quite literally pop it out
+	// and then append the extra args
+	for len(extra_args) > 0 {
+		var arg string
+		arg, extra_args = pop(extra_args)
+
+		if arg == "--" {
+			break
+		}
+
+		args = append(args, arg)
 	}
 
 	// check if next exists
@@ -190,10 +227,10 @@ func execCommandAndNext(command *toml.Tree, scripts *toml.Tree) {
 
 		// check if next script is array
 		if _, ok := nextScript.([]interface{}); ok {
-			execCommand(nextScript.([]interface{}))
+			execCommand(nextScript.([]interface{}), extra_args)
 		} else {
 			// next script is struct
-			execCommandAndNext(nextScript.(*toml.Tree), scripts)
+			execCommandAndNext(nextScript.(*toml.Tree), scripts, extra_args)
 		}
 	}
 }
